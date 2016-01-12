2b7edd8360a19b5f581db8155883387fc6e33c26
davidyou-frog <bmfrog2@gmail.com>
2016-01-12 23:43:10 +0900

    append before_login state

--- FileTreeModel/client/js/app.js
+++ FileTreeModel/client/js/app.js
@@ -1,6 +1,30 @@
-var mainApp = angular.module('mainApp',[]);
+var mainApp = angular.module('mainApp',['ui.router']);
 
-mainApp.run( [function(){
+mainApp.config([ '$stateProvider','$urlRouterProvider',
+function($stateProvider,$urlRouterProvider) {
 
+  $stateProvider
+    .state('before_login', {
+		url        : '/before_login',
+		templateUrl: 'view/before_login.html',
+    })
+    ;
+	
+	$urlRouterProvider.otherwise('before_login');
+	
+}]);
+
+
+mainApp.run( ['$rootScope', function($rootScope){
+
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+        console.log( 'Event $stateChangeStart' );
+		console.log( '  event = ', event );
+		console.log( '  toState = ', toState );
+		console.log( '  toParams = ', toParams );
+		console.log( '  fromState = ', fromState );
+		console.log( '  fromParams = ', fromParams );
+    });
+	
 }]);
 
